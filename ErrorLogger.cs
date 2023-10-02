874a8cf338d4f1ea3f9afc129d2b5e16f6c7dcd2
Azukimochi <Azukimochi25565@gmail.com>
2023-10-02 20:26:16 +0900

    first commit

---
+++ ErrorLogger.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace io.github.azukimochi 
+{
+    public class ErrorLogger : MonoBehaviour
+    {
+        private ChatGPTWindow chatGPT; // ChatGPTのスクリプトへの参照
+
+        private void OnEnable()
+        {
+            Application.logMessageReceived += HandleLog;
+        }
+
+        private void OnDisable()
+        {
+            Application.logMessageReceived -= HandleLog;
+        }
+
+        void HandleLog(string logString, string stackTrace, LogType type)
+        {
+            if (type == LogType.Error || type == LogType.Exception)
+            {
+                // エラーメッセージをChatGPTに送信
+                SendErrorMessageToChatGPT(logString + "\n" + stackTrace);
+            }
+        }
+
+        void SendErrorMessageToChatGPT(string errorMessage)
+        {
+            if (chatGPT == null)
+            {
+                chatGPT = GetComponent<ChatGPTWindow>();
+            }
+
+            // ChatGPTにエラーメッセージを送信
+            //chatGPT.AskChatGPT(errorMessage, (response) =>
+            //{
+            //    // 応答を処理するコードをここに書く
+            //    Debug.Log("ChatGPT Response: " + response);
+            //});
+        }
+    }
+
+}
